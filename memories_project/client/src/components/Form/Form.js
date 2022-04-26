438a058ee46350672fb599ba63599cd57fdf8697
Mohd Naeem <naeem.mohd@hotmail.com>
2022-04-25 23:23:59 -0500

    Added Update functionality

--- memories_project/client/src/components/Form/Form.js
+++ memories_project/client/src/components/Form/Form.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useStyles from './styles';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
-import { createPost } from '../../actions/posts';
+import { useDispatch, useSelector } from 'react-redux';
+import { createPost, updatePost } from '../../actions/posts';
 
-const Form = () => {
+const Form = ({currentId, setCurrentId}) => {
     const classes = useStyles();
+
     const [postData, setPostData] = useState({
         creator: '',
         title: '',
@@ -14,19 +15,35 @@ const Form = () => {
         tags: '',
         selectedFile: ''        
     });
+
+    const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(post) setPostData(post);
+    }, [post]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(createPost(postData));
+        
+        if(currentId){
+            dispatch(updatePost(currentId, postData));
+        }else{
+            dispatch(createPost(postData));
+        }
+        clear();
     };
+    
     const clear = () => {
-
+        setCurrentId(0);
+        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
     };
+
     return (
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant='h6'>Creating a memory</Typography>
+                <Typography variant='h6'>{currentId ? 'Editing' : 'Creating'} a memory</Typography>
                 <TextField 
                     name='creator' label='Creator' variant='outlined' fullWidth 
                     value={postData.creator} 
--- memories_project/server/controllers/posts.js
+++ memories_project/server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async(req, res) => {
@@ -8,6 +9,7 @@ export const getPosts = async(req, res) => {
         res.status(404).json({ message: error.message });
     }
 };
+
 export const createPost = async (req, res) => {
     const post = req.body;
     const newPost = new PostMessage(post);
@@ -18,3 +20,19 @@ export const createPost = async (req, res) => {
         res.status(409).json({ message: error.message });
     }
 };
+
+export const updatePost = async (req, res) => {
+    const {id: _id} = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)){
+        return res.status(404).send('No post with that Id');
+    }
+    
+    try {
+        const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true});
+        res.json(updatedPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
