d99826687e3bb472db3d4f3837e54ffbceff384a
Mohd Naeem <naeem.mohd@hotmail.com>
2022-04-28 23:13:10 -0500

    added application security, code refactoring etc

--- memories_project/client/src/components/Form/Form.js
+++ memories_project/client/src/components/Form/Form.js
@@ -6,19 +6,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { createPost, updatePost } from '../../actions/posts';
 
 const Form = ({currentId, setCurrentId}) => {
-    const classes = useStyles();
-
-    const [postData, setPostData] = useState({
-        creator: '',
-        title: '',
-        message: '',
-        tags: '',
-        selectedFile: ''        
-    });
-
+    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
     const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
-
     const dispatch = useDispatch();
+    const classes = useStyles();
 
     useEffect(() => {
         if(post) setPostData(post);
@@ -32,13 +23,14 @@ const Form = ({currentId, setCurrentId}) => {
         }else{
             dispatch(createPost(postData));
         }
-        clear();
+        clear();        
     };
     
     const clear = () => {
         setCurrentId(0);
         setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
     };
+    
 
     return (
         <Paper className={classes.paper}>
--- memories_project/server/index.js
+++ memories_project/server/index.js
@@ -4,17 +4,19 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
-app.use('/posts', postRoutes); // http://localhost:5000/posts
+app.use('/posts', postRoutes); // http://localhost:5000/posts 
 
 // https://www.mongodb.com/cloud/atlas
-const CONNECTION_URL = 'mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.s8cwg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000; 
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
